ffeba0bcf449e7f5f66ddfdd44cb3b0ee1975759
Jurgen-Stegeman <87040815+Thoredo@users.noreply.github.com>
2025-03-18 15:20:55 +0100

    refactored assignment 14

--- 2-sterren/opdracht-14-fibonacci.py
+++ 2-sterren/opdracht-14-fibonacci.py
@@ -3,6 +3,4 @@ number_b = 1
 
 for i in range(15):
     print(number_a)
-    temporary_number = number_b
-    number_b += number_a
-    number_a = temporary_number
+    number_a, number_b = number_b, number_a + number_b
